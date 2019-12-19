ad278881b4a25c40ed1217e81e80bdb5c1d90990
Diego <diego_sanchez-brunete_apps1ma1819@cev.com>
2019-12-19 14:40:04 +0100

    seguir con android

--- app_jose/app/src/main/java/com/example/app_jose/MainActivity.java
+++ app_jose/app/src/main/java/com/example/app_jose/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.app_jose;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner desplegable = (Spinner) findViewById(R.id.spinner2);
+
+        desplegable.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.d("sdf", "funciona");
+
+            }
+        });
+
     }
 }
